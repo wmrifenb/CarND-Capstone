cfd96571658401d2ab4d27206be0393bde6559b5
Betabot <hatsmagee@gmail.com>
2017-10-27 21:11:12 +1100

    Save

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -2,6 +2,7 @@ import rospy
 from styx_msgs.msg import TrafficLight
 import tensorflow as tf
 import cv2
+from model import Model
 
 class TLClassifier(object):
     def __init__(self):
@@ -25,22 +26,22 @@ class TLClassifier(object):
         """
 
         # What... is the light
-        if False:
+        if True:
             if traffic_light_state_truth == TrafficLight.RED:
                 self.red_image_number += 1
-                cv2.imwrite("red/image_" + str(self.red_image_number) + ".png", image)
+                cv2.imwrite("new/red/image_" + str(self.red_image_number) + ".png", image)
 
             if traffic_light_state_truth == TrafficLight.YELLOW:
                 self.yellow_image_number += 1
-                cv2.imwrite("yellow/image_" + str(self.yellow_image_number) + ".png", image)
+                cv2.imwrite("new/yellow/image_" + str(self.yellow_image_number) + ".png", image)
             
             if traffic_light_state_truth == TrafficLight.GREEN:
                 self.green_image_number += 1
-                cv2.imwrite("green/image_" + str(self.green_image_number) + ".png", image)
+                cv2.imwrite("new/green/image_" + str(self.green_image_number) + ".png", image)
             
             if traffic_light_state_truth == TrafficLight.UNKNOWN:
                 self.unknown_image_number += 1
-                cv2.imwrite("unknown/image_" + str(self.unknown_image_number) + ".png", image)
+                cv2.imwrite("new/none/image_" + str(self.unknown_image_number) + ".png", image)
         
         # Save
         return TrafficLight.UNKNOWN
